cb3ddc1faae3c69caa8428eaaf3b5c1772927826
MaazRaeen <Maazraeen42@gmail.com>
2026-09-19 20:03:43 +0530

    Add Header and Footer components; refactor App structure

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function Footer(){
+    return(
+        <div className="footer">
+            <footer>&copy;{new Date().getFullYear()} Maaz Raeen</footer>
+        </div>
+        
+    );
+}
+export default Footer
